8875a80f6ccb07d2dcdbfa3b72eccf48b89beea2
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-25 19:58:20 +0100

    Created core config file

---
+++ index.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+  // Parser
+  // http://eslint.org/docs/user-guide/configuring#specifying-parser
+  parser: 'esprima',
+
+  // Parser Options
+  // http://eslint.org/docs/user-guide/configuring#specifying-parser-options
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+
+  // Environments
+  // http://eslint.org/docs/user-guide/configuring#specifying-environments
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+
+  // Rules
+  // http://eslint.org/docs/user-guide/configuring#configuring-rules
+  rules: {
+    'strict': 'error'
+  },
+
+  // Extend
+  // http://eslint.org/docs/user-guide/configuring#extending-configuration-files
+  extends: [
+    './config/base'
+  ].map(require.resolve)
+}
