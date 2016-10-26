af617d653180f14c18e535532a544f23d3cc8ead
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-26 14:50:11 +0100

    Removed unnecessary quotes

--- configs/eslint/strict.js
+++ configs/eslint/strict.js
@@ -5,6 +5,6 @@ module.exports = {
 
     // Require or disallow strict mode directives
     // http://eslint.org/docs/rules/strict
-    'strict': [ 'error', 'never' ]
+    strict: [ 'error', 'never' ]
   }
 }
