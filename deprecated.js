2ca45fa53e94be946a910ab1d011b5f5b2d9b07c
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-26 07:34:00 +0100

    Created deprecated rules file for testing

---
+++ deprecated.js
@@ -0,0 +1,10 @@
+module.exports = [
+
+  // ESLint http://eslint.org/docs/rules/#deprecated
+  'no-native-reassign',
+  'no-negated-in-lhs',
+  'no-spaced-func',
+
+  // Import Plugin
+  'import/imports-first'
+]
