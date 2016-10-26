0d31c35cae903cc9c9fed2bfdfe7ef105afd74ce
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-26 06:09:27 +0100

    Added error rules

--- configs/eslint/errors.js
+++ configs/eslint/errors.js
@@ -1,9 +1,127 @@
-// http://eslint.org/docs/rules/#possible-errors
 module.exports = {
+
+  // http://eslint.org/docs/rules/#possible-errors
   rules: {
 
     // Disallow assignment operators in conditional expressions
     // http://eslint.org/docs/rules/no-cond-assign
-    // 'no-cond-assign': 'error'
+    'no-cond-assign': 'off',
+
+    // Disallow the use of console
+    // http://eslint.org/docs/rules/no-console
+    'no-console': 'off',
+
+    // Disallow constant expressions in conditions
+    // http://eslint.org/docs/rules/no-constant-condition
+    'no-constant-condition': 'off',
+
+    // Disallow control characters in regular expressions
+    // http://eslint.org/docs/rules/no-control-regex
+    'no-control-regex': 'off',
+
+    // Disallow the use of debugger
+    // http://eslint.org/docs/rules/no-debugger
+    'no-debugger': 'off',
+
+    // Disallow duplicate arguments in function definitions
+    // http://eslint.org/docs/rules/no-dupe-args
+    'no-dupe-args': 'off',
+
+    // Disallow duplicate keys in object literals
+    // http://eslint.org/docs/rules/no-dupe-keys
+    'no-dupe-keys': 'off',
+
+    // Disallow duplicate case labels
+    // http://eslint.org/docs/rules/no-duplicate-case
+    'no-duplicate-case': 'off',
+
+    // Disallow empty character classes in regular expressions
+    // http://eslint.org/docs/rules/no-empty-character-class
+    'no-empty-character-class': 'off',
+
+    // Disallow empty block statements
+    // http://eslint.org/docs/rules/no-empty
+    'no-empty': 'off',
+
+    // Disallow reassigning exceptions in catch clauses
+    // http://eslint.org/docs/rules/no-ex-assign
+    'no-ex-assign': 'off',
+
+    // Disallow unnecessary boolean casts
+    // http://eslint.org/docs/rules/no-extra-boolean-cast
+    'no-extra-boolean-cast': 'off',
+
+    // Disallow unnecessary parentheses
+    // http://eslint.org/docs/rules/no-extra-parens
+    'no-extra-parens': 'off',
+
+    // Disallow unnecessary semicolons
+    // http://eslint.org/docs/rules/no-extra-semi
+    'no-extra-semi': 'off',
+
+    // Disallow reassigning function declarations
+    // http://eslint.org/docs/rules/no-func-assign
+    'no-func-assign': 'off',
+
+    // Disallow variable or function declarations in nested blocks
+    // http://eslint.org/docs/rules/no-inner-declarations
+    'no-inner-declarations': 'off',
+
+    // Disallow invalid regular expression strings in RegExp constructors
+    // http://eslint.org/docs/rules/no-invalid-regexp
+    'no-invalid-regexp': 'off',
+
+    // Disallow irregular whitespace outside of strings and comments
+    // http://eslint.org/docs/rules/no-irregular-whitespace
+    'no-irregular-whitespace': 'off',
+
+    // Disallow calling global object properties as functions
+    // http://eslint.org/docs/rules/no-obj-calls
+    'no-obj-calls': 'off',
+
+    // Disallow calling some Object.prototype methods directly on objects
+    // http://eslint.org/docs/rules/no-prototype-builtins
+    'no-prototype-builtins': 'off',
+
+    // Disallow multiple spaces in regular expressions
+    // http://eslint.org/docs/rules/no-regex-spaces
+    'no-regex-spaces': 'off',
+
+    // Disallow sparse arrays
+    // http://eslint.org/docs/rules/no-sparse-arrays
+    'no-sparse-arrays': 'off',
+
+    // Disallow template literal placeholder syntax in regular strings
+    // http://eslint.org/docs/rules/no-template-curly-in-string
+    'no-template-curly-in-string': 'off',
+
+    // Disallow confusing multiline expressions
+    // http://eslint.org/docs/rules/no-unexpected-multiline
+    'no-unexpected-multiline': 'off',
+
+    // Disallow unreachable code after return, throw, continue, and break statements
+    // http://eslint.org/docs/rules/no-unreachable
+    'no-unreachable': 'off',
+
+    // Disallow control flow statements in finally blocks
+    // http://eslint.org/docs/rules/no-unsafe-finally
+    'no-unsafe-finally': 'off',
+
+    // Disallow negating the left operand of relational operators
+    // http://eslint.org/docs/rules/no-unsafe-negation
+    'no-unsafe-negation': 'off',
+
+    // Require calls to isNaN() when checking for NaN
+    // http://eslint.org/docs/rules/use-isnan
+    'use-isnan': 'off',
+
+    // Enforce valid JSDoc comments
+    // http://eslint.org/docs/rules/valid-jsdoc
+    'valid-jsdoc': 'off',
+
+    // Enforce comparing typeof expressions against valid strings
+    // http://eslint.org/docs/rules/valid-typeof
+    'valid-typeof': 'off'
   }
 }
+
