ce55a1a94d1ad5856c586da1b9bfb9a9a3ae0724
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-26 09:25:03 +0100

    Added deprecated react rules

--- deprecated.js
+++ deprecated.js
@@ -1,10 +1,18 @@
 module.exports = [
 
-  // ESLint http://eslint.org/docs/rules/#deprecated
+  // ESLint
+  // http://eslint.org/docs/rules/#deprecated
   'no-native-reassign',
   'no-negated-in-lhs',
   'no-spaced-func',
 
   // Import Plugin
-  'import/imports-first'
+  // https://github.com/benmosher/eslint-plugin-import/blob/dc3609f895f5ad390d88fd9925dce40960117930/src/index.js#L35
+  'import/imports-first',
+
+  // React Plugin
+  // https://github.com/yannickcr/eslint-plugin-react/blob/e1697996ff494bf584057418e2974ece7f3d99d8/index.js#L3
+  'react/no-comment-textnodes',
+  'react/require-extension',
+  'react/wrap-multilines'
 ]
