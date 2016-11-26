3a87e6b4a3c249e996f7525bf55ee5a1226c2dcd
Matthew Wagerfield <matthew@wagerfield.com>
2016-11-26 16:45:32 +0000

    Disabled module system rules

--- rules/import/module-systems.js
+++ rules/import/module-systems.js
@@ -10,13 +10,13 @@ module.exports = {
      * Report AMD require and define calls
      * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-amd.md
      */
-    'import/no-amd': 'error',
+    'import/no-amd': 'off',
 
     /**
      * Report CommonJS require calls and module.exports or exports.*
      * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-commonjs.md
      */
-    'import/no-commonjs': 'error',
+    'import/no-commonjs': 'off',
 
     /**
      * No Node.js builtin modules
@@ -28,6 +28,6 @@ module.exports = {
      * Report potentially ambiguous parse goal (script vs. module)
      * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/unambiguous.md
      */
-    'import/unambiguous': 'error'
+    'import/unambiguous': 'off'
   }
 }
