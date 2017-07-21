bbce544eb495894d48d803c0f62c8d193da0c449
Matthew Wagerfield <matthewwagerfield@Matthews-MacBook-Air.local>
2017-07-21 10:50:18 +0100

    Removed legacy flowtype rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,6 @@ module.exports = {
     './flowtype.js'
   ],
   rules: {
-    'max-len': 'off',
-    // Remove when issue resolved
-    // https://github.com/gajus/eslint-plugin-flowtype/issues/165
-    'flowtype/require-valid-file-annotation': 'off'
+    'max-len': 'off'
   }
 }
